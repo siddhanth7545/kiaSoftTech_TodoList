340abde49d294cad4813528817b0d98444c13e76
siddhanth7545 <siddhanth7545@gmail.com>
2023-05-31 22:02:26 +0530

    submited

--- src/AcademyManager/AddAcademy.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const AddAcademy = () => {
-  return (
-    <h1>hi hello</h1>
-  )
-}
-
-export default AddAcademy
--- src/services/AxiosInstance.jsx
+++
@@ -1,5 +0,0 @@
-import axios from "axios";
-const axiosInstance=axios.create({
-    baseURL:"http://localhost:8082"
-})
-export default axiosInstance;
